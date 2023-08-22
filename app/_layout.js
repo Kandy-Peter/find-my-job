38f59de98387a72087c349d0da0854e8468c6b78
kandy <kandypeter03@gmail.com>
2023-08-22 15:21:56 +0200

    Finalize the welcome style

--- app/_layout.js
+++ app/_layout.js
@@ -1,8 +1,27 @@
 import { Stack } from "expo-router";
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync()
 
 const Layout = () => {
+  const [isLoaded] = useFonts({
+    DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+    DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+    DMRegular: require('../assets/fonts/DMSans-Regular.ttf')
+  })
+
+  const onLayoutRootView = useCallback(async () => {
+    if (isLoaded) {
+      await SplashScreen.hideAsync()
+    }
+  }, [isLoaded])
+
+  if(!isLoaded) return null;
+
   return (
-    <Stack/>
+    <Stack onLayout={onLayoutRootView} />
   )
 }
 
